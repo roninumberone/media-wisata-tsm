f127c20d1c564157cec2303ad40f61c765d115d9
roni.romdhoni <jrkancil513@gmail.com>
2025-05-11 18:30:22 +0700

    versi 0.0.0 merubah kontak dan navbar

--- js/main.js
+++ js/main.js
@@ -1,163 +1,193 @@
 (function ($) {
-    "use strict";
-    
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+  "use strict";
 
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+    function toggleNavbarMethod() {
+      if ($(window).width() > 992) {
+        $(".navbar .dropdown")
+          .on("mouseover", function () {
+            $(".dropdown-toggle", this).trigger("click");
+          })
+          .on("mouseout", function () {
+            $(".dropdown-toggle", this).trigger("click").blur();
+          });
+      } else {
+        $(".navbar .dropdown").off("mouseover").off("mouseout");
+      }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+  });
 
-    // Main News carousel
-    $(".main-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Main News carousel
+  $(".main-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: true,
+    loop: true,
+    center: true,
+  });
+
+  // Tranding carousel
+  $(".tranding-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 2000,
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+  });
+
+  // Carousel item 1
+  $(".carousel-item-1").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+  });
+
+  // Carousel item 2
+  $(".carousel-item-2").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 30,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+      0: {
         items: 1,
-        dots: true,
-        loop: true,
-        center: true,
-    });
-
-
-    // Tranding carousel
-    $(".tranding-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 2000,
+      },
+      576: {
         items: 1,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left"></i>',
-            '<i class="fa fa-angle-right"></i>'
-        ]
-    });
+      },
+      768: {
+        items: 2,
+      },
+    },
+  });
 
-
-    // Carousel item 1
-    $(".carousel-item-1").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
+  // Carousel item 3
+  $(".carousel-item-3").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 30,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+      0: {
         items: 1,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ]
-    });
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
 
-    // Carousel item 2
-    $(".carousel-item-2").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 30,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
-
-
-    // Carousel item 3
-    $(".carousel-item-3").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 30,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
-
-    // Carousel item 4
-    $(".carousel-item-4").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 30,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            },
-            1200:{
-                items:4
-            }
-        }
-    });
-    
+  // Carousel item 4
+  $(".carousel-item-4").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 30,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
 })(jQuery);
 
+//   tanggal dan waktu
+
+var tw = new Date();
+if (tw.getTimezoneOffset() == 0) a = tw.getTime() + 7 * 60 * 60 * 1000;
+else a = tw.getTime();
+tw.setTime(a);
+var tahun = tw.getFullYear();
+var hari = tw.getDay();
+var bulan = tw.getMonth();
+var tanggal = tw.getDate();
+var hariarray = new Array(
+  "Minggu,",
+  "Senin,",
+  "Selasa,",
+  "Rabu,",
+  "Kamis,",
+  "Jum'at,",
+  "Sabtu,"
+);
+var bulanarray = new Array(
+  "Januari",
+  "Februari",
+  "Maret",
+  "April",
+  "Mei",
+  "Juni",
+  "Juli",
+  "Agustus",
+  "September",
+  "Oktober",
+  "Nopember",
+  "Desember"
+);
+document.getElementById("tanggalwaktu").innerHTML =
+  hariarray[hari] + " " + tanggal + " " + bulanarray[bulan] + " " + tahun;
